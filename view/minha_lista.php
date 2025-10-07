470e9c8e1f4e134ea85738947463c3024d80f3c1
Pedro Rocha <pedro@email.com>
2025-10-07 17:06:51 -0300

    inicio do código html em minha_lista

---
+++ view/minha_lista.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minhas Receitas</title>
+    <link rel="stylesheet" href="./css/minhas_receitas.css">
+</head>
