6446f28609cae0ed87ba20364e0e1b9c9360e473
José Matheus Nagata Kulibaba <josematheusnagatak@hotmail.com>
2025-11-23 19:44:37 -0300

    Adiciona charset UTF-8 na conexão PDO e ajusta configuração de nomes

--- model/conexao.php
+++ model/conexao.php
@@ -7,10 +7,12 @@ $banco = "swipesdereceitas";
 global $pdo;
 
 try {
-    $pdo = new PDO("mysql:dbname=".$banco.";host=".$localhost, $user, $password);
+    $pdo = new PDO("mysql:dbname=".$banco.";host=".$localhost.";charset=utf8mb4", $user, $password);
+    
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("set names utf8mb4");
 } catch (PDOException $e) {
     echo "erro: " .$e->getMessage();
 }
-
+?>
 ?>
